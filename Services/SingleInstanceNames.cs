8e296d22d37a0b356dc0ba2f9028bc47193b9b1b
Mehmet Ali Şeker <maliseker2005@gmail.com>
2026-08-04 23:12:39 +0300

    feat: implement "About" page, acceptance ping service, and dialogs for device/schedule management

--- Services/SingleInstanceNames.cs
+++ Services/SingleInstanceNames.cs
@@ -5,4 +5,6 @@ public static class SingleInstanceNames
     public const string ManagementUi = @"Local\NetworkHealthMonitor.ManagementUi";
 
     public const string Tray = @"Local\NetworkHealthMonitor.Tray";
+
+    public const string Worker = @"Global\NetworkHealthMonitor.Worker";
 }
